cbfd6866b72938a5f058e205b98da6035ecf334b
Victor Leite <victor_leite@outlook.com.br>
2022-11-13 11:14:10 -0300

    implement monster http fetch and filter input

--- src/App.js
+++ src/App.js
@@ -1,32 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-
-
   constructor() {
-    super()
+    console.log("constructor");
+    super();
 
-    this.state = { 
-      firstName: 'Victor', 
-      lastName: 'Leite' }
+    this.state = {
+      monsters: [],
+      filterString: '',
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return {
+              monsters: users,
+              filteredMonsters: users,
+            };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
   }
 
   render() {
+    console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(this.state.filterString)
+  );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>My name is {this.state.firstName} Leite</p>
-          <button onClick={() => {
-            this.setState({ firstName: 'Raimundo' })
-          }}>Change Name</button>
-        </header>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const filterValue = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { filterString: filterValue };
+            });
+          }}
+        ></input>
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
-
 }
 
 export default App;
