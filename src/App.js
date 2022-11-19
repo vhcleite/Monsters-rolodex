bfc6689cbd0be2b3b5b36e73d5fb650f6d92c326
Victor Leite <victor_leite@outlook.com.br>
2022-11-19 18:54:21 -0300

    add page title

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           onChangeHanler={onSearchChange}
